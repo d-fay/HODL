45e14f8dd1d27512d675e376583394ed64f18b6d
d-fay <aero@aeros-MacBook-Pro.local>
2018-05-05 11:38:31 -0600

    add some comments to explain some questionble logic

--- hodl/exchanges/BinanceHelperAPI.py
+++ hodl/exchanges/BinanceHelperAPI.py
@@ -41,6 +41,26 @@ def print_binance_ascii():
           '\____/|_|_| |_|\__,_|_| |_|\___\___|')
 
 
+def calc_price_of_market(mrkt_ticker, aggressiveness=0.5):
+    """
+    Calculates the price of a market using the spread
+        aggressiveness = 1.0 : uses the highest bid (best when selling)
+        aggressiveness = 0.0 : uses the lowest ask (best when buying)
+        aggressiveness = 0.5 : middle of spread
+
+    :param aggressiveness: Ratio used to control how aggressive
+    :type aggressiveness: float (zero to one)
+    :return: calculated price for asset
+    :rtype: float
+    """
+    depth = client.get_order_book(symbol=mrkt_ticker)
+    highest_bid = float(depth['bids'][0][0])
+    lowest_ask = float(depth['asks'][0][0])
+    spread = highest_bid - lowest_ask
+    btc_price = float((spread * aggressiveness) + lowest_ask)
+    return round(btc_price, 8)
+
+
 def print_formatted_balance(asset_ticker, asset_balance, asset_free_bal, mkt_btc_value):
 
     mkt_str = ' --- {0: <6} ---  '.format(asset_ticker)
@@ -73,6 +93,12 @@ def print_binance_balances():
 
             if asset_balance > NEAR_ZERO_BALANCE:
                 mkt_ticker = '{}BTC'.format(asset_ticker)
+
+                # There has to be a better way to handle this. Can we obtain a
+                # list of market tickers for each coin we hold and conditionally
+                # query them? Right now a handfull of coins that show up in this
+                # list do not have {}BTC markets. So the below query throws an
+                # error when we provide an invalid market ticker.
                 try:
                     mkt_price = calc_price_of_market(mkt_ticker)
                     mkt_btc_value = mkt_price * asset_balance
@@ -97,26 +123,6 @@ def print_binance_balances():
           .format(account_value_btc))
     print('------------------------------------------------------'
           '------------------------------------------------------')
-
-
-def calc_price_of_market(mrkt_ticker, aggressiveness=0.5):
-    """
-    Calculates the price of a market using the spread
-        aggressiveness = 1.0 : uses the highest bid (best when selling)
-        aggressiveness = 0.0 : uses the lowest ask (best when buying)
-        aggressiveness = 0.5 : middle of spread
-
-    :param aggressiveness: Ratio used to control how aggressive
-    :type aggressiveness: float (zero to one)
-    :return: calculated price for asset
-    :rtype: float
-    """
-    depth = client.get_order_book(symbol=mrkt_ticker)
-    highest_bid = float(depth['bids'][0][0])
-    lowest_ask = float(depth['asks'][0][0])
-    spread = highest_bid - lowest_ask
-    btc_price = float((spread * aggressiveness) + lowest_ask)
-    return round(btc_price, 8)
 
 
 def get_binance_account_value():
